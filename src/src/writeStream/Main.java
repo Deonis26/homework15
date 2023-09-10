5e13b25a0e370ec3c30abe8e3783ae93f1c34096
Denis <85250266+Deonis26@users.noreply.github.com>
2023-09-10 23:16:44 +0300

    решение задачи

---
+++ src/src/writeStream/Main.java
@@ -0,0 +1,52 @@
+package writeStream;
+
+import java.io.*;
+
+public class Main {
+
+    static String a = "The Rostovs knew everybody in Moscow. The old count had money enought that year, " +
+            "as all his estates had been remortgaged, and so Nicholas, acquiring a troller of his own, " +
+            "very stylish riding breeches of the latest cut, such as no one else yet had in Moscow, and boots " +
+            "of the latest fashion, with extremely pointed toes and small silver spues, passed his very gaily.";
+
+    public static void main(String[] args) {
+        File file = new File("textFolder/newFile.txt");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            String text = a;
+            fileOutputStream.write(text.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        File file2 = new File("/Users/denislameko/Desktop/Test", "text.txt");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file2);
+            fileWriter.write(a);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        File file3 = new File("/Users/denislameko/Desktop/Test/text.txt");  //можно записать в одну строку try(BufferedReader bufferReadered = new BufferedReader(new FileReader(new File("путь"))))
+        try (FileReader fileReader = new FileReader(file3)) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String text =bufferedReader.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file4 = new File("textFolder/newFile.txt");
+        try(BufferedReader bufferedReader= new BufferedReader(new FileReader(new File("textFolder/newFile.txt")))){
+            String text =bufferedReader.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
